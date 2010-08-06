4871f83fd4125b5c2726ed6306f9912483a4e633
Michal Taszycki <michal@applicake.com>
2010-08-06 12:34:05 +0200

    dropbox remote aliases

--- zsh-custom/git.zsh
+++ zsh-custom/git.zsh
@@ -19,3 +19,24 @@ alias gpdm='git push dropbox master'
 alias gldc='git pull dropbox $(current_branch)'
 alias gpdc='git push dropbox $(current_branch)'
 
+function git_init_dropbox_bare(){
+  GIT_DIR=~/Dropbox/git_private/$1/$2.git git init --bare 
+}
+
+function git_add_remote_dropbox(){
+  git add remote dropbox ~/Dropbox/git_private/$1/$2.git 
+}
+
+
+function git_clone_from_dropbox(){
+  git clone --origin dropbox ~/Dropbox/git_private/$1/$2.git
+}
+
+alias gidp='git_init_dropbox_bare projects'
+alias gidw='git_init_dropbox_bare applicake'
+
+alias gcdp='git_clone_from_dropbox projects'
+alias gcdw='git_clone_from_dropbox applicake'
+
+alias gardp='git_add_remote_dropbox projects'
+alias gardw='git_add_remote_dropbox applicake'
