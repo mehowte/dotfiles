31009c3eae5e7af0a2462ee6ab36bb49930053d7
Michal Taszycki <michal@applicake.com>
2011-06-11 16:30:25 +0200

    path shortcuts added

---
+++ zsh-custom/path_shortcuts.zsh
@@ -0,0 +1,8 @@
+alias cdw='cd ~/code/work'
+alias cdp='cd ~/code/private'
+alias cds='cd ~/code/spikes'
+alias cdg='cd ~/code/github'
+alias cdm='cd ~/code/github/mehowte'
+alias cdd='cd ~/code/github/mehowte/dotfiles'
+
+alias mvd='cdd && mvim'
